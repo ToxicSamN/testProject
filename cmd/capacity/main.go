7f27c69453031bb41a6e5b813704b24471b4c503
Sammy <sammy.d.shuck@gmail.com>
2020-01-09 09:17:51 -0700

    Adding project test files

---
+++ cmd/capacity/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"gitlab.nordstrom.com/huggin/stacktracker"
+	"github.com/vmware/govmomi"
+)
+
+
+func main() {
+	return
+}
---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ToxicSamN/testProject/internal/stringutil"
+)
+
+func main() {
+	fmt.Println("Hello, World, Test Project")
+	fmt.Println(Reverse("Hello, World, Test Project"))
+}
